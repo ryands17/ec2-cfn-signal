2bbe731d3de8544491ee3a763a7747a74d82472f
Ryan <dsouza.ryan.11@gmail.com>
2021-03-04 17:14:37 +0530

    initial commit

--- test/ec2-cfn-signal.test.ts
+++ test/ec2-cfn-signal.test.ts
@@ -1,13 +1,13 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as Ec2CfnSignal from '../lib/ec2-cfn-signal-stack';
+// import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+// import * as cdk from '@aws-cdk/core';
+// import * as Ec2CfnSignal from '../lib/ec2-cfn-signal-stack';
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new Ec2CfnSignal.Ec2CfnSignalStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
-});
+// test('Empty Stack', () => {
+//     const app = new cdk.App();
+//     // WHEN
+//     const stack = new Ec2CfnSignal.Ec2CfnSignalStack(app, 'MyTestStack');
+//     // THEN
+//     expectCDK(stack).to(matchTemplate({
+//       "Resources": {}
+//     }, MatchStyle.EXACT))
+// });
