7845e047e2d4979731bb12763a1f0b1a143de7bc
Ryan <dsouza.ryan.11@gmail.com>
2021-03-05 14:06:22 +0530

    add tests

--- lib/ec2-cfn-signal-stack.ts
+++ lib/ec2-cfn-signal-stack.ts
@@ -1,9 +1,47 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
+import * as ec2 from '@aws-cdk/aws-ec2'
+import * as iam from '@aws-cdk/aws-iam'
 
 export class Ec2CfnSignalStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    // Create a VPC to deploy the instance to
+    const vpc = new ec2.Vpc(this, 'signalVPC', {
+      maxAzs: 1,
+      cidr: '192.168.0.0/24',
+      subnetConfiguration: [
+        { name: 'public', subnetType: ec2.SubnetType.PUBLIC, cidrMask: 25 },
+      ],
+      natGateways: 0,
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+    })
+
+    // The Ubuntu EC2 instance
+    const instance = new ec2.Instance(this, 'signalTest', {
+      vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: ec2.MachineImage.latestAmazonLinux(),
+      init: ec2.CloudFormationInit.fromElements(
+        ec2.InitPackage.yum('gcc'),
+        ec2.InitPackage.yum('gcc-c++'),
+        ec2.InitPackage.yum('make'),
+        ec2.InitCommand.shellCommand(
+          'curl -fsSL https://rpm.nodesource.com/setup_14.x | bash -'
+        ),
+        ec2.InitCommand.shellCommand('yum install -y nodejs')
+      ),
+      initOptions: { timeout: cdk.Duration.minutes(5) },
+      userDataCausesReplacement: true,
+    })
+
+    // Add the policy to access EC2 without SSH
+    instance.role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')
+    )
   }
 }
--- test/ec2-cfn-signal.test.ts
+++ test/ec2-cfn-signal.test.ts
@@ -1,13 +1,60 @@
-// import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-// import * as cdk from '@aws-cdk/core';
-// import * as Ec2CfnSignal from '../lib/ec2-cfn-signal-stack';
+import {
+  expect as expectCDK,
+  haveResourceLike,
+  SynthUtils,
+} from '@aws-cdk/assert'
+import * as cdk from '@aws-cdk/core'
+import * as Ec2CfnSignal from '../lib/ec2-cfn-signal-stack'
 
-// test('Empty Stack', () => {
-//     const app = new cdk.App();
-//     // WHEN
-//     const stack = new Ec2CfnSignal.Ec2CfnSignalStack(app, 'MyTestStack');
-//     // THEN
-//     expectCDK(stack).to(matchTemplate({
-//       "Resources": {}
-//     }, MatchStyle.EXACT))
-// });
+test('Snapshot matches the resources correctly', () => {
+  const stack = createStack()
+  expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot()
+})
+
+test('VPC and public subnet is created', () => {
+  const stack = createStack()
+
+  expectCDK(stack).to(
+    haveResourceLike('AWS::EC2::VPC', {
+      CidrBlock: '192.168.0.0/24',
+      EnableDnsHostnames: true,
+      EnableDnsSupport: true,
+      InstanceTenancy: 'default',
+      Tags: [],
+    })
+  )
+
+  expectCDK(stack).to(
+    haveResourceLike('AWS::EC2::Subnet', {
+      CidrBlock: '192.168.0.0/25',
+      VpcId: {},
+      AvailabilityZone: {},
+      MapPublicIpOnLaunch: true,
+      Tags: [],
+    })
+  )
+})
+
+test('EC2 instance is created with the correct properties', () => {
+  const stack = createStack()
+
+  expectCDK(stack).to(
+    haveResourceLike('AWS::EC2::Instance', {
+      AvailabilityZone: {},
+      IamInstanceProfile: {},
+      ImageId: {},
+      InstanceType: 't2.micro',
+      SecurityGroupIds: [],
+      SubnetId: {},
+      Tags: [],
+      UserData: {
+        'Fn::Base64': {},
+      },
+    })
+  )
+})
+
+function createStack() {
+  const app = new cdk.App()
+  return new Ec2CfnSignal.Ec2CfnSignalStack(app, 'MyTestStack')
+}
